db9f15c07c3122862032da885c91a05055c495cf
Andrew Attilio <aattilio@pm.me>
2023-07-18 14:30:35 -0700

    enabled homepage search and a working results page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from csvToDict import csvToDict
 
 app = Flask(__name__)
 
 ingredientPairings = csvToDict('flavor_bible_full.csv')
 
+
 @app.route("/")
 def index():
-    pairings = ingredientPairings.get('CINNAMON')
-    return render_template("index.html", pairings=pairings)
+    main = 'blackberries'
+    pairings = ingredientPairings.get(main)
+    allMains = list(ingredientPairings.keys())
+    return render_template("index.html", pairings=pairings, main=main.capitalize(), allMains=allMains)
+
+
+@app.route("/results")
+def results():
+    main = request.args.get("choice")
+    pairings = ingredientPairings.get(main.lower())
+    allMains = list(ingredientPairings.keys())
+    return render_template("results.html", pairings=pairings, main=main, allMains=allMains)
--- csvToDict.py
+++ csvToDict.py
@@ -13,12 +13,15 @@ def csvToDict(pairingsCSV):
         ingredientReader = csv.reader(csvfile)
         pairingList = []
         prevKey = 'ACHIOTE SEEDS'
+        badKeywords = ('Season:', 'Techniques:', 'Taste:', 'Volume:', 'Weight:', 'Flavor Aff', 'Botanical rel', 'Tips:')
         for row in ingredientReader: # for each row in the file
             if row[0] != prevKey: # if current main is a new ingredient
-                ingredientMatches.update({prevKey: pairingList}) # update the dictionary with {main: pairing list}
+                ingredientMatches.update({prevKey.lower(): pairingList}) # update the dictionary with {main: pairing list}
                 pairingList = [] # clear the list for new pairings
-            pairingList.append(row[1]) # add the pairing to the pairingList
+            if row[1].startswith(badKeywords):
+                pass
+            else:
+                pairingList.append(row[1]) # add the pairing to the pairingList
             prevKey = row[0] # update prevKey to the current main
 
     return ingredientMatches
-
